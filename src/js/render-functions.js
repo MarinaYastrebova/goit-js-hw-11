b5c3cb6703f92db77460053635f76784e9f15924
Marina Yastrebova <yasstreb@gmail.com>
2025-10-18 23:03:07 +0300

    instal and repository formatting

---
+++ src/js/render-functions.js
@@ -0,0 +1,9 @@
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export function createGallery(images) {}
+function clearGallery() {}
+function showLoader() {}
+function hideLoader() {}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+import getImagesByQuery from './js/pixabay-api';
+import createGallery from './js/render-functions';
